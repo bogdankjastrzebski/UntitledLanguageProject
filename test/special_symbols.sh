174f584a891a19fb92172c0d25a72efc06fd392e
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-01-26 02:17:23 +0100

    get_symbol

---
+++ test/special_symbols.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+special_symbol () {
+	text=$1
+	i=$(("${#text}" - 1))
+	word=""
+	while [ $i -ge 0 ]; do
+		char="${text:$i:1}"
+		if [ "$char" = $'\x5c' ]; then # \
+			echo "$word $i"
+		elif [ "$char" = $'\x20' ]; then # space
+			exit
+		else
+			word=$char$word
+		fi
+		i=$((i - 1))
+	done 
+}
+
+special_symbol "$1"
