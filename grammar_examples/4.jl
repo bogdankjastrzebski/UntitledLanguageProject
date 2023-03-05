ec3563a4f3d7bd3a26c8267280f792c6e0c1afe2
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-03-05 20:16:22 +0100

    grammar examples

--- grammar_examples/4.jl
+++ grammar_examples/4.jl
@@ -1,4 +1,4 @@
-## Automatic Differentiation
+# Automatic Differentiation
 
 # Firstly, automatic differentiation is for functions!
 
@@ -18,10 +18,11 @@ function fib(n)
 function forward(w, b, x)
 	return w * x + b # all is broadcasted!
 
-J: forward.part(w, b).derivative(x) # this one? 
+J: forward.part(w, b).derivative(x) # this one? Super verbose 
 J: forward.part(w, b).der(x)
+J: forward.p(w, b).d(x) # maybe this one? 
 J: forward.part(w, b)'(x)
-J: forward(w, b, _)'(x) # this one?
+J: forward(w, b, _)'(x) # this one? looks cool
 
 # J: (x -> forward(w, b, x))'(x) # Julias Notation is still one of the longest
 
@@ -30,12 +31,15 @@ J: forward(w, b, _)'(x) # this one?
 # the product of it and another vector is simply 
 # element-wise multiplication and is linear in complexity.
 
- 
+# Derivatives/Jacobians are between two normed normed vector spaces.
+# The vectors can be anything from numbers, arrays to functions etc.
+# For obvious reasons it is not always possible, to fill out whole interface.
 
+# What king of things do we want to do with jacobians? What is the interface?
 
-
-
-
+# Firstly, we want jacobian vector product. 
+# Secondly, we want jacobian jacobian product.
+# Maybe we need only jacobian jacobian product?
 
 
 
