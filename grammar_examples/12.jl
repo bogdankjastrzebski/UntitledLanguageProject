39ffd0e1e97f3ecccaf043d1b6d9268a82bfc782
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-03-13 20:55:09 +0100

    slicing and indexing arrays

---
+++ grammar_examples/12.jl
@@ -0,0 +1,22 @@
+
+# Indexing
+
+let fib(n Int)
+	a Int: 1
+	b Int: 0
+	for _ in 0..n
+		a, b : b, a+b
+	return b
+
+arr : [1, 2, 3, 4, 5]
+
+arr[1..] + arr[..-1]
+
+arr[0..2..]
+
+plot(arr[.., 0], arr[..,1]) 
+
+
+
+
+
