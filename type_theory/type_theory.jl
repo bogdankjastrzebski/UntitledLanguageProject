b4f598bcc75a15e04cf93e7eca87dab0b085e869
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-03-30 23:46:31 +0200

    type theory

---
+++ type_theory/type_theory.jl
@@ -0,0 +1,32 @@
+
+# The basic proposition is this:
+# If we can prove, that if 
+# * t1(a) (is equal to true)
+# then  
+# * t2(f(a)) 
+# Then we have a type theory.
+
+string?(x) = true if x isa String else False
+
+let foo(x string?) string?
+	return ... 
+
+# That means, that we should be able to prove, that
+# if the entry is a string, the output is a string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
