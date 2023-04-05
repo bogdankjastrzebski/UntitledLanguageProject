c4fe7f5f25d925acf239c6b8a2da8830a573258e
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-04-05 13:00:04 +0200

    grammar examples

--- grammar_examples/alternative/2.jl
+++ grammar_examples/alternative/2.jl
@@ -53,6 +53,7 @@ let sort_str(string string?) string? {
 
 
 
+
 let sort_str(string string?) string? {
 	counts : [zero(ui32?) for _ in 0..256];
 	for letter in string {
@@ -61,10 +62,24 @@ let sort_str(string string?) string? {
 	str : [zero(char?) for _ in 0..256];
 	ind : zero(i32);	
 	for char, count in enumerate(counts) { 
-		while (count ≠ 0) { str[ind] : char }
+		while (count ≠ 0) { str[ind] : char };
 	};
 	return str;
 };
+
+let sort_str(string string?) string? {
+	counts : [zero(ui32?) for _ in 0..256]
+	for letter in string {
+		counts[letter] +: 1
+	}	
+	str : [zero(char?) for _ in 0..256]
+	ind : zero(i32)
+	for char, count in enumerate(counts) { 
+		while (count ≠ 0) { str[ind] : char }
+	}
+	return str
+}
+
 
 let sort_str(string string?) string?
 	
@@ -80,21 +95,136 @@ let sort_str(string string?) string?
 
 	return str
 	
-	quota : begin
-	  quote
-		a : 0
-		b : 1
-		for _ in 0..n	
-			a, b : b, a+b
-		return b 
+quota : begin
+  quote
+	something
+
+quota : quote { something }
+
+quota : quote { something; };
+
+data : begin
+  sql
+	select * from data_frame
+	where ... 
+
+data : sql {
+	select * from data_frame
+	where ... 
+}
+
+data : sql {
+	select * from data_frame
+	where ... 
+};
+
+
+let fib(n i32) i32 {
+	a i32 : 1
+	b i32 : 0
+	for _ 0..n { a, b : b, a+b }
+	return b
+}
+
+fib(n i32) i32 : {
+	a, b : 1, 0
+	for _ 0..n { a, b : b, a+b }
+	return b
+}
+
+fib(n i32) i32 : n < 2 ? n | fib(n-1) + fib(n-2)
+fib(n i32) i32 : n if n < 2 else fib(n-1) + fib(n-2)
+fib(n i32) i32 : n < 2 then n else fib(n-1) + fib(n-2)
+
+let fib(n i32) i32
+	a, b : 1, 0
+	for _ 0..n
+		a, b : b, a+b
+	return b
+
+fib(n i32) i32 : begin
+	a, b : 1, 0
+	for _ 0..n
+		a, b : b, a+b 
+	return b
+
+fib : func(n i32) i32
+	a, b : 1, 0
+	for _ 0..n
+		a, b : b, a+b
+	return b
+
+
+if pred {
+	something
+} pred2 {
+
+} else {
+	something
+}
+
+
+fib(n i32) i32 : {
+	a : 0
+	b : 1
+	for i 0..n {
+		a, b : b, a+b
+	}
+	return b
+}
+
+fib(n i32) i32 : n < 2 ? n | fib(n-1) + fib(n-2)
+
+asdf : {
+	s : 0
+	for i 0..n {
+		s +: 1/i
+	}	
+	return s
+}
+
+data : {sql select * from df
+			where something } 
+
+func : lambda(x) {
+	x : x + 1
+	return x
+}
+
+
+func(x i32) i32 : {
+	for _ 0..n {
+		x += i32(1)
+	}
+	return x
+}
+
+let func(x i32) i32
+	for _ 0..n	
+		x += i32(1) 
+	return x
+
+let foo(x @i32) @i32
+	a : begin
+		return x + x
+	return a
+
+let bar(df @data_frame) @string
+	data : sql
+		select name from df
+		where value > 0
+	return cat(data)
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 	
-	data : begin
-	  sql
-		select * from data_frame
-		where ... 
-
-
-
-
-
-
