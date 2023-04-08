8b1ecc568783cae43e5edcf72dcd6a6157ecfbde
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-04-08 23:06:02 +0200

    type theory

--- type_theory/implementation.py
+++ type_theory/implementation.py
@@ -10,8 +10,23 @@
 #
 #
 
-def foo(x: int) -> int:
-	return x + x
+# A function has a rule assotiated with it.
+# It says, if the input is obeys a, then the output obeys b. 
 
-print(foo(3)) 
+class Nil:
+	pass
+
+class SuperObject:
+	def __init__(self, value):
+		self.value = value	
+	def __getattr__(self, name):
+		if hasattr(self, name):
+			return self.__getattr__(name)
+		return Nil()
+
+class SuperFunction:
+	def __init__(self, name, operation): 
+		pass
+
+
 
