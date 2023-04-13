8f0d96d64ae4cb7ed99dfed92806d66f952f20bb
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-04-13 22:24:33 +0200

    destroyable objects

--- grammar_examples/destroyable_objects.jl
+++ grammar_examples/destroyable_objects.jl
@@ -19,7 +19,32 @@ It is possible, not to know a field value at any point in time.
 We can definitelly previously know, and now don't.
 We can also know now, but not previously.
 
+
+Let's say, that we have a program A and a 
+set of symbols, whose value is unknown.
+
+We want to express the program through only the things we don't know.
+Consider a lambda calculus, where compute everything up to the application
+of an unknown symbol:
+
 """
+
+begin with unknown n
+	s : 0
+	for i in 0..fib(5)	
+		s +: i*n
+	s	
+	
+"""
+Above, we can:
+* calculate fib(3)
+* unroll the loop
+* state, that s = 0+1*n+2*n+3*n+4*n
+		then s = (1+2+3+4)*n = 10*n 
+"""
+	
+
+
 
 
 
