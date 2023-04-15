dee3dcf8f0d4ee5bbf72af80f63cde01f7b63872
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-04-15 13:36:09 +0200

    naming scheme

--- naming_scheme.jl
+++ naming_scheme.jl
@@ -29,6 +29,90 @@ let fib(n integer_32) integer_32
 	return b
 
 	
+Benefits of:
+* short names: i32
+  upsides:
+	* popular among skilled programmers
+	* fast to write
+	* unpopular names
+  downsides:
+	* we cannot express new names easily using this formula.
+	* it can be used only for primitive types.
+* CamelCaseFullNames:
+  upsides:
+	* for all new types as well
+	* everyone knows, what the type represents
+	* easy to differentiate between types and other variables
+  downsides:
+	* very long to write
+	* long to read
+
+
+let fib(n Integer32) Integer32
+	a Integer32: 1
+	b Integer32: 0
+	c Integer32: 0
+	while n > 0
+		c : a
+		a : b
+		b : c
+		n -: 1
+	return b
+
+let fib(n Int32) Int32
+	a Int32: 1
+	b Int32: 0
+	c Int32: 0
+	while n > 0
+		c : a
+		a : b
+		b : c
+		n -: 1
+	return b
+
+
+let fib(n i32) i32
+	a i32: 1
+	b i32: 0
+	c i32: 0
+	while n > 0
+		c : a
+		a : b
+		b : c
+		n -: 1
+	return b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
