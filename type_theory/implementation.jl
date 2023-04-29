387b0755140271290ac137cb8f49efb73b476921
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-04-29 21:33:57 +0200

    implementation of a simple lambda calculus with maybe known monad

--- type_theory/implementation.jl
+++ type_theory/implementation.jl
@@ -1,15 +1,50 @@
 
+abstract type MaybeKnown end
 
-struct LazyFunction
-	func::Function
+mutable struct CallCons <: MaybeKnown
+	operator
+	operand
+end 
+
+mutable struct Value <: MaybeKnown
+	name::Symbol
+	known::Bool
+	value
+end
+Value(name::Symbol) = Value(name, false, nothing)
+Value(arg) = Value(:literal, true, arg)
+
+function is_known(value::Value)
+	return value.known
 end
 
-struct UnknownValue
-	
-
-function (l::LazyFunction)(args...)
-	return l.func(args...)
+function is_known(::CallCons)
+	return false
 end
+
+function (fun::MaybeKnown)(arg::MaybeKnown)
+	if is_known(fun) && is_known(arg)
+		return fun.value(arg.value) 
+	end
+	return CallCons(fun, arg)		
+end
+
+
+
+
+
+
+
+# function (fun::CallCons)(arg)
+# 	return CallCons(fun, arg)
+# end
+# 
+# function (fun::Value)(arg::CallCons)
+# 	return CallCons(fun, arg)
+# end	
+
+
+
 
 
 
