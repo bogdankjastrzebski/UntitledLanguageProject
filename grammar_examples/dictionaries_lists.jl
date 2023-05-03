8587a646d376095d6afe7b65739929c3c09a8f72
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-05-03 23:34:25 +0200

    dicts

---
+++ grammar_examples/dictionaries_lists.jl
@@ -0,0 +1,26 @@
+
+# Dictionaries
+#
+# Dictionary is ment to be a "swiss army knife" of our language.
+# It is meant to be a scripting thing, that you do not want to 
+# use in highly optimized code, but it should cause no trouble.
+# "PLUG & PLAY"
+# Features:
+# * by default, it is not typed
+# * it is able to store everything as a key and value
+# * IT IS ORDERED
+dict : {} 
+dict : { 'a' : 1, 'b' : 2, 'c' : 3 }
+dict : { 
+	'a' : 1, 
+	'b' : 2, 
+	'c' : 3
+}
+
+dict[ind] 
+dict.get(key, default)
+dict.nth(ind) # ordered!
+# it is ordered in order to be able to discover the order in which elements has been added, for instance. 
+
+
+
