f083fd53ed0389df00415d66abf7f6e63a933771
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-05-08 22:45:00 +0200

    examples of compile time values, why not

--- grammar_examples/compile_time_values.jl
+++ grammar_examples/compile_time_values.jl
@@ -17,6 +17,33 @@ fib100 Int : fib(100) # 100 is a compile time value, not an int
 
 # What will the code above do? 
 
+# generally, making it to be int is very difficult, but possible. 
+# it can generated sub-optimal code implicitly
+
+
+# I would say, that the julia's model is actually better.
+# It is better, to explicitly ask for types.
+#
+
+let fib(n)
+    a i32 : 0
+    b i32 : 0
+    for _ in 0..n
+        a, b : b, a+b
+    return b
+
+let fib(n)
+    a : i32(1)
+    b : i32(0)
+    for _ in 0..n
+        a, b : b, a+b
+    return b
+
+# The first grammar is cleaner, but the second one is simpler.
+
+
+
+
 
 
 
