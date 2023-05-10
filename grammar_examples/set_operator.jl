cd03c099ed2e4b664091c52449a349eb52188397
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-05-10 22:04:58 +0200

    assigning operators

---
+++ grammar_examples/set_operator.jl
@@ -0,0 +1,47 @@
+
+# to make the language easy, let's make the following convention:
+a : 0 # creates a
+a : 1 
+# changes a to 1
+
+# Inside a function, names from outer scopes have to be allowed.
+# We should also be able to set them. 
+
+let fib(n)
+    a i32
+    b i32
+    a : 1
+    b : 0 
+    for _ in 0..n
+        a, b : b, a+b
+    return b
+
+let fib(n)
+    a : 0
+    b : 1
+    for _ in 0..n
+        a, b <- b, a+b
+    return b
+
+let fib(n)
+    a :: 0
+    b :: 1
+    for _ in 0..n
+        a, b : b, a+b
+    return b
+
+let fib(n)
+    a <- 0
+    b <- 1
+    for _ in 0..n
+        a, b : b, a+b
+    return b
+
+let fib(n)
+    a := 0
+    b := 1
+    for _ in 0..n
+        a, b = b, a + b
+    return b
+
+
