0121c5bb65060e74e1012c02aa73fe7121c08278
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-06-09 23:02:48 +0200

    example

---
+++ new_grammar/fancy.jl
@@ -0,0 +1,55 @@
+
+
+
+code := begin
+    s := 0
+    for i in 0..10
+        s += i
+    return s
+ 
+code := (begin
+             s := 0
+             for i in 0..10
+                 s += i
+             return s)
+
+func := (lambda(x) x+x)
+
+func := (lambda (x)
+            x *= 2
+            return x+x)
+
+pipe := df
+    .where(c'alpha < 0)
+    .select(sum(c'(beta)))
+
+
+
+let foo(x: Integer): Integer
+    y := zero(x)
+    for i in 0..n
+        y += i
+    return y
+
+foo = add!(x, y)
+
+
+let fib(n: Integer): Integer
+    a, b: Return = 1, 0
+    while n > 0
+        a, b = b, a + b
+        n -= 1 
+    return b 
+
+        
+        
+    
+
+
+    
+     
+
+    
+
+
+
