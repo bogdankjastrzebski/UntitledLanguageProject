f281068eab3323b805e8fdb313e36a00daedd4cd
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-08-05 00:33:48 +0200

    couple examples

--- new_grammar/newnew.jl
+++ new_grammar/newnew.jl
@@ -1,5 +1,4 @@
 
-
 let fib(n: I32): I32
     a, b: I32 = 0, 1
     while n > 0
@@ -7,8 +6,23 @@ let fib(n: I32): I32
         n -= 1
     return b
 
-
 true  := x ↦ y ↦ x
 false := x ↦ y ↦ y
+
+function fib: Integer → Integer
+    0 ↦ 1
+    1 ↦ 1
+    n ↦ fib(n-1) + fib(n-2)
+
+function fib(n: Integer): Integer
+    match n
+        0 ↦ 1
+        1 ↦ 1
+        n ↦ fib(n-1) + fib(n-2)
+
+function fib(n: Integer): Integer
+    rev: Int × Int × Int → Int = (a, b, n) ↦ b if n == 0 else rev(b, a + b, n - 1)
+    # rev := (a, b, n) ↦ b if n == 0 else rev(b, a+b, n-1)
+    return rev(1, 0, n)
 
 
