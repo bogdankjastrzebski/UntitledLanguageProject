2fda6a33b9ee44b7c370f0d47a934adbc4767883
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-08-09 23:34:55 +0200

    verynew

---
+++ new_grammar/verynew.jl
@@ -0,0 +1,27 @@
+
+
+: for type theory
+= for setting
+
+n: Int
+n = 0
+n: Int = 0
+n := 0 # type infered
+
+f: I ↦ I
+
+true: Bool = x ↦ y ↦ x
+false: Bool = x ↦ y ↦ y
+
+pair: T → T → Bool → T = \
+    a ↦ b ↦ p ↦ (p a b)
+
+first: (Bool → T) → T = \
+    p ↦ (p true)
+
+second: (Bool → T) → T = \
+    p ↦ (p false)
+
+(pair 3 4): Bool → T
+
+(first (pair 3 4))
