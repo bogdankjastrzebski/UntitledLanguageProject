0a7667e7d0ecd94ca96ebf4ee9dbc3f9cf512978
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-08-11 23:53:31 +0200

    fib

--- new_grammar/verynew.jl
+++ new_grammar/verynew.jl
@@ -28,4 +28,26 @@ second: (Bool → T) → T = \
 
 f: I → I → I = x ↦ y ↦ x + y
 
+foo: Int × Int → Int = (x, y) ↦ x + y
+    
+let foo(x: Int, y: Int): Int
+    return x + y
+
+let fib(n: Int): Int
+    a, b, c: I32 = 1, 0, 0
+    while n > 0
+        c = a
+        a = b
+        b = c
+        n -= 1
+    return b
+
+
+
+
+        
+
+ 
+
+
 
