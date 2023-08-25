d506d42864f7a8a973c40bb9d7fcb17ff8a135af
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-08-25 23:21:12 +0200

    update

--- new_grammar/agda-inspired.jl
+++ new_grammar/agda-inspired.jl
@@ -32,9 +32,9 @@ using Traits
 
 if n == 0
     return 0
-   n == 1
+  n == 1
     return 1
-   else
+  else
     return fib(n-1) + fib(n-2)
 
 case n
@@ -42,4 +42,6 @@ case n
     1 => 1
     n => fib(n-1) + fib(n-2)
  
+
+
 
