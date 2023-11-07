0858ce5f3dd1f079c8460d28c872274637fcd598
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-11-07 22:22:55 +0100

    new grammar

--- new_grammar/super_new.jl
+++ new_grammar/super_new.jl
@@ -2,6 +2,35 @@
 # Let's design something inbetween
 # Agda, but more like sql
 
-foo: X → Y = x ↦ 
-    
+fib : Int → Int
+fin(n)
+    a, b : Int = 1, 0
+    for _ in 0..n
+        a, b = b, a + b
+    return b
+
+fib(n: Int): Int
+    a, b := 1, 0
+    for _ in 0..n
+        a, b = b, a + b
+    return b
+
+# the following are equivalent:
+alpha = beta
+alpha
+    beta
+
+# So we can do:
+dataframe
+    sql select * from df
+    where something
+
+code
+  quote
+    a := 0
+    b := 1
+
+foo(x)
+    return x + x
+
 
