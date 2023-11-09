8ac2a8f4e8429c86f291922e0c300e8ae9f9dc90
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-11-10 00:02:09 +0100

    new

--- new_grammar/super_new.jl
+++ new_grammar/super_new.jl
@@ -41,3 +41,29 @@ foo a b #macro
     $a $b
            
 
+let fib(n: Int): Int
+    a, b := 1, 0
+    for _ in 0..n
+        a, b = b, a + b
+    return b
+
+let fib: Int → Int = n ↦ n if n ≤ 1 else fib(n-1) + fib(n-2)
+
+let fib: Int → Int
+    fib(0) = 0
+    fib(1) = 1
+    fib(suc(suc(n))) = fib(suc(n)) + fib(n)
+
+if a ≡ 0
+    print(a)
+  else
+    print(b)
+    
+if a ≤ 2
+    print(2)
+  a ≤ 4
+    print(4)
+  else
+    print(5)
+
+
