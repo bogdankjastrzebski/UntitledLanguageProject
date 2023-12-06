62ba7dd00a84b397a08f2afb3a02279172cf2292
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-12-06 23:55:03 +0100

    third option

--- new_grammar/superb.jl
+++ new_grammar/superb.jl
@@ -1,7 +1,7 @@
 
 # A minimal language with decidable allocation.
 
-fib(x: i32): i32
+const fib(n: i32): i32
     a, b := 1, 0
     for _ 0..n
         a, b = b, a + b
@@ -11,4 +11,7 @@ fib: i32 -> i32
 fib 0 = 0
 fib 1 = 1
 fib (1+ (1+ n)) = fib (1+ n) + fib n
+
+fib(n: i32): i32 = n if n < 2 else fib(n-1) + fib(n-2)
+
 
