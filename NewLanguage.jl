7ff7c95424b5f3138586c1ae96ef31b2c00967da
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2024-02-15 23:35:09 +0100

    New Language

---
+++ NewLanguage.jl
@@ -0,0 +1,38 @@
+
+# The New Language
+
+"""
+Lisp, without brackets, with dependent types, implemented as lambdas.
+Language designed for building code, compiling it(, saving) and running.
+"""
+
+fun : {X : Set} -> (x : X) -> L x
+fun 0 = ...  
+fun (suc x) = ...
+
+# Mało programistycznie, może tak:
+
+fun{X : Set}(x: X): L x
+  match x
+    0       ...
+    (suc x) ...
+ 
+# Równoważne definicje:
+fun(x, y) = ... 
+fun(x, y)
+    ...
+fun{X Y : Set}(x : X, y : Y)
+fun{X : Set}{Y : Set}(x : X, y : Y)
+    ...
+fun{X : Set}{Y : Set}((x , y) : Tuple X Y)
+    ...
+
+# Nie równoważne definicje:
+f(x)(y):    X -> Y -> Z = ...
+f(x, y): Tuple X Y -> Z = ...
+
+# Równoważne:
+foo(x) = bar(x)
+foo(x; bar=bar) = bar(x)
+foo(bar=bar)(x) = bar(x)
+
